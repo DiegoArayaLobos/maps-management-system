c4a39fadfeccd2d96635ac8253f45f7f3a974354
Diego Araya Lobos <diego.araya.lobos@gmail.com>
2022-02-03 19:26:01 -0300

    MMS 0.1.0

--- src/config/firebaseConfig.tsx
+++ src/config/firebaseConfig.tsx
@@ -1,1 +1,18 @@
-//Private
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/functions";
+import "firebase/firestore";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/storage";
+
+export const firebaseConfig = {
+    //Private
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+firebase.analytics();
+
+export default firebase;
